2074b8125bf7b75bfe86d4354ec7bacb662c5846
muazaqeel <24086656@siswa.um.edu.my>
2025-11-11 10:35:37 +0800

    Update W05E01.java

--- Exercise2025/src/W05/W05E01.java
+++ Exercise2025/src/W05/W05E01.java
@@ -9,9 +9,9 @@ package W05;
  * @author jites
  */
 public class W05E01 {
-    public static void main(String args[]){
-        for (int i = 1; i <= 20; i++){
-            if (i % 2 == 0){
+     public static void main(String[] args) {
+        for (int i = 1; i <= 20; i++) {
+            if (i % 2 == 0) {
                 System.out.println(i);
             }
         }
